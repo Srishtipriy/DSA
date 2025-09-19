2e3e6279a4a85f51d0cf3ec2813c2d519a3de1d4
srishti <srishti.priya.vns@gmail.com>
2025-09-19 19:47:41 +0530

    Time: 3 ms (65.61%) | Memory: 10 MB (91.59%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -23,11 +23,11 @@ public:
             ans.push_back(board);
             return ;
         }
-        for(int j= 0 ; j<n ; j++){                  //j here means "col"
-            if(isSafe(board , row , j , n )){                   //if true hua toh hi proceed and put "Q"
-                board[row][j] = 'Q';
+        for(int col= 0 ; col<n ; col++){                  //j here means "col"
+            if(isSafe(board , row , col , n )){                   //if true hua toh hi proceed and put "Q"
+                board[row][col] = 'Q';
                 solve(board, row+1 , n , ans);           //backtrack
-                board[row][j] = '.';               
+                board[row][col] = '.';               
             }
         }     
     }
