03f5fc2632b572afd7a99ca9dee0ee67bf0fa334
Srishti Priya <srishti.priya.vns@gmail.com>
2025-09-21 11:41:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (22.10%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int fibo(int n, vector<int>& dp){
+    int solve(int n, vector<int>& dp) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
 
-        if(n<= 1)   return n;
-        if(n==2)   return 1;
+        if (dp[n] != -1) return dp[n];
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = fibo(n-1,dp) + fibo(n-2,dp) + fibo(n-3,dp);
-        return dp[n];
-
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp) + solve(n-3, dp);
     }
-    int tribonacci(int n) {
-        vector<int> dp(n+1);
 
-        for(int i=0; i<=n ; i++){
-            dp[i]=-1;
-        }
-        int f= fibo(n,dp);
-        return f;
+    int tribonacci(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
     }
 };
