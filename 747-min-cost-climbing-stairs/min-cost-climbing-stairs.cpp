9495b8742459ad6dc47185fbcb7f4a268e5e1351
Srishti Priya <srishti.priya.vns@gmail.com>
2025-09-22 20:56:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (74.54%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int solve(vector<int>& cost, int n) {
         vector<int> dp(n+1);                             //step 1 
-        dp[0] = cost[0];                                 //step 2-- base cases ko dp m likho
-        dp[1] = cost[1];
+        int prev2 = cost[0];                             //step 2-- base cases ko dp m likho
+        int prev1 = cost[1];
         
         for(int i =2 ;i<n ; i++){
-            dp[i] = cost[i] +min(dp[i-1] , dp[i-2]);            // recusrion wala format dp m likh do
+            int curr = cost[i] +min(prev1 , prev2); 
+            prev2 = prev1;
+            prev1 = curr;                    // recusrion wala format dp m likh do
         }
-    return min(dp[n-1], dp[n-2]);               // this line from main fun jaha ans ko call kar arhe the
+    return min(prev2, prev1);               // this line from main fun jaha ans ko call kar arhe the
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
