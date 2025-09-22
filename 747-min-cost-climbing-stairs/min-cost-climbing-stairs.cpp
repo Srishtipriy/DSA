304e4519a81aef1772bf4c12cb07def5e8768e98
Srishti Priya <srishti.priya.vns@gmail.com>
2025-09-22 20:06:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.86%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& cost, int i, vector<int> & dp) {
-        if (i >= cost.size()) return 0;   // Top par pahunch gaye
+    int solve(vector<int>& cost, int n) {
+        vector<int> dp(n+1);                             //step 1 
+        dp[0] = cost[0];                                 //step 2-- base cases ko dp m likho
+        dp[1] = cost[1];
         
-        if(dp[i] != -1)  return dp[i];
-
-        int one = cost[i] + solve(cost, i+1, dp);       // Ek step aur do step dono try karo
-        int two = cost[i] + solve(cost, i+2, dp);
-
-        dp[i] =min(one, two);
-        return dp[i];
+        for(int i =2 ;i<n ; i++){
+            dp[i] = cost[i] +min(dp[i-1] , dp[i-2]);            // recusrion wala format dp m likh do
+        }
+    return min(dp[n-1], dp[n-2]);               // this line from main fun jaha ans ko call kar arhe the
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        int n= cost.size();
-        vector<int> dp (n+1, -1);
-        int ans =min(solve(cost, 0 ,dp), solve(cost, 1,dp));     // Tum 0 ya 1 step se start kar sakte ho
-        return ans;
+        int n =cost.size();
+        return solve(cost, n);     // Tum 0 ya 1 step se start kar sakte ho
     }
 };
