9c4cf69bb7018ddf32002a4ec57c12aed83c73a0
Srishti Priya <srishti.priya.vns@gmail.com>
2025-10-08 23:48:44 +0530

    Time: 8 ms (13.79%) | Memory: 146.7 MB (89.04%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -9,35 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include <queue>
-using namespace std;
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (!root) return 0; 
-
-        queue<pair<TreeNode*, int>> q;  // q to store nodes along with their depth
-        q.push({root, 1});  // Start with the root node at depth 1
         
-        while (!q.empty()) {
-            auto [node, depth] = q.front();  // Get the front node and its depth
-            q.pop();
-            
-            if (!node->left && !node->right) {          // If this is a leaf node, return its depth
-                return depth;
-            }
-            
-            // If there is a left child, add it the q with depth + 1
-            if (node->left) {
-                q.push({node->left, depth + 1});
-            }
-            
-            // If right child, add it to the q with depth + 1
-            if (node->right) {
-                q.push({node->right, depth + 1});
-            }
-        }
+        if(root == NULL)  return 0;
         
-        return 0;  // This line will never be reached if the tree is valid
+        int leftDepth = minDepth(root->left);
+        int rightDepth = minDepth(root->right);
+        
+        if(root->left == NULL && root->right == NULL)       // If the both subtrees are empty.
+            return 1;
+        
+        if(root->left == NULL)                    //If left subtree isempty, return depth adding 1 to it
+            return 1 + rightDepth;
+        if(root->right == NULL)
+            return 1 + leftDepth;
+        
+        return min(leftDepth, rightDepth) + 1;
     }
 };
