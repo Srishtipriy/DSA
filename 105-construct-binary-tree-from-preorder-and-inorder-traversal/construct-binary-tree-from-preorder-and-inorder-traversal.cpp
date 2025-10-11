2dfb5b5fd47593391a9bd6ffa2ac47b0f5aadee2
Srishti Priya <srishti.priya.vns@gmail.com>
2025-10-11 21:46:33 +0530

    Time: 3 ms (75.21%) | Memory: 27.5 MB (35.70%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,36 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    int pre_index = 0;
-    unordered_map<int,int> indices;
 public:
-    TreeNode* dfs(vector<int>& preorder, int left, int right){
+TreeNode* constructTree(vector <int> & preorder, int preStart, int preEnd, vector <int> & inorder, int inStart, int inEnd, map <int, int> & mp) {
 
-        if( left>right )          return NULL;
+  if (preStart > preEnd || inStart > inEnd)             return NULL;
 
-        int rootval = preorder[pre_index++];
-        TreeNode* root = new TreeNode(rootval);
+  TreeNode* root = new TreeNode(preorder[preStart]);
+  int ele = mp[root -> val];                           //ele is the position of root in inorder.
+  int nEle = ele - inStart;
 
-        int mid = indices[rootval];      //indices hashmap to find where this root is located in the inorder array
+  root -> left = constructTree(preorder, preStart+1, preStart + nEle, inorder,
+  inStart, ele - 1, mp);
+  root -> right = constructTree(preorder, preStart + nEle + 1, preEnd, inorder, 
+  ele + 1, inEnd, mp);
 
-        root->left = dfs(preorder, left, mid-1);
-        root->right = dfs(preorder, mid+1, right);
+  return root;
+}
 
-        return root;
-    }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for(int i = 0 ; i<inorder.size(); i++){
-            indices[inorder[i]] = i;                        // store indices for O(1) lookup
-        }        
-        return dfs(preorder, 0, inorder.size()-1);
-    }
+TreeNode* buildTree(vector < int > & preorder, vector < int > & inorder) {
+  int preStart = 0, preEnd = preorder.size() - 1;
+  int inStart = 0, inEnd = inorder.size() - 1;
+
+  map < int, int > mp;
+  for (int i = inStart; i <= inEnd; i++) {
+    mp[inorder[i]] = i;
+  }
+
+  return constructTree(preorder, preStart, preEnd, inorder, inStart, inEnd, mp);
+}
 };
