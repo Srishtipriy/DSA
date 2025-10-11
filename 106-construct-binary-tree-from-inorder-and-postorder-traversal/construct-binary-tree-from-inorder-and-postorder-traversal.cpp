b58dfaee3a0b9fb8205944fe07eb0194850a880c
Srishti Priya <srishti.priya.vns@gmail.com>
2025-10-11 22:32:29 +0530

    Time: 5 ms (36.52%) | Memory: 28.8 MB (7.55%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,30 +11,27 @@
  */
 
 // postorder = Left → Right → Root
-// inorder   = Left → Root → Right
-
+// inorder   = Left → Root → Right                  inorder  = [9, 3, 15, 20, 7]  n = 5
 class Solution {
 public:
-    int findPosition(vector<int>& inorder, int element, int n) {
+    void createMapping( vector<int>& inorder, map<int ,int>& nodeToindex , int n) {
         for (int i = 0; i < n; i++) {
-            if (inorder[i] == element)
-                return i;
-        }
-        return -1;
+            //map[1st digit in inorder]           nodeToindex[9] = 0-->{9: 0} =>nodeToindex[3] = 1-->{9: 0, 3: 1}
+            nodeToindex[inorder[i]] =i;                 //inorder of i ko map kar do index ke saath
+        }               
     }
-
     TreeNode* solve(vector<int>& postorder, vector<int>& inorder, int& postindex, int inorderStart,
-        int inorderEnd, int n) {
-            
+        int inorderEnd, int n, map<int, int>& nodeToindex) {
+
         if (postindex < 0 || inorderStart > inorderEnd)
             return NULL;
 
         int element = postorder[postindex--];
         TreeNode* root = new TreeNode(element);
-        int position = findPosition(inorder, element, n);
+        int position = nodeToindex[element];
 
-        root->right = solve(postorder, inorder, postindex, position + 1, inorderEnd, n);
-        root->left  = solve(postorder, inorder, postindex, inorderStart, position - 1, n);
+        root->right = solve(postorder, inorder, postindex, position + 1, inorderEnd, n, nodeToindex );
+        root->left  = solve(postorder, inorder, postindex, inorderStart, position - 1, n, nodeToindex);
 
         return root;
     }
@@ -42,6 +39,8 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = postorder.size();
         int postindex = n - 1;
-        return solve(postorder, inorder, postindex, 0, n - 1, n);
+        map<int,int> nodeToindex;
+        createMapping(inorder, nodeToindex , n);
+        return solve(postorder, inorder, postindex, 0, n - 1, n, nodeToindex);
     }
 };
