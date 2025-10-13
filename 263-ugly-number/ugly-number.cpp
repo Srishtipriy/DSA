2fbbbd6edc65611e12a439a9988ed043135a0af3
Srishti Priya <srishti.priya.vns@gmail.com>
2025-10-13 22:22:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (44.12%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -4,27 +4,19 @@ public:
         if(n==1)        return true;
         if(n<=0)        return false;
 
-        int ans= 0;
         while(n>1){
             if(n%2== 0){
                 n/=2;
-                ans= 1;
-                continue;
             }
-            if(n%3== 0){
+            else if(n%3== 0){
                 n/=3;
-                ans= 1;
-                continue;
             }
-            if(n%5== 0){
+            else if(n%5== 0){
                 n/=5;
-                ans= 1;
-                continue;
             }
             else
                 return false;
         }
-        if(ans>0)       return true;
-        else     return false;
+        return true;
     }
 };
