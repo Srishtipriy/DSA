5066e4c281b9cdbc0b1ba1dead4e787ecce4a4bf
Srishti Priya <srishti.priya.vns@gmail.com>
2025-10-17 00:49:26 +0530

    Time: 2 ms (4.91%) | Memory: 10.9 MB (41.37%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,26 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int> &ans){
-        if(root ==NULL)
-            return ;
-        ans.push_back(root->val);
-        solve(root-> left , ans);
-        solve(root-> right ,ans); 
+    vector<int> getPreorder(TreeNode* root) {
+        vector<int> preorder;
+        TreeNode* cur = root;
+
+        while (cur != NULL) {
+            if (cur->left == NULL) {
+                preorder.push_back(cur->val);
+                cur = cur->right;
+            } else {
+                TreeNode* prev = cur->left;
+                while (prev->right && prev->right != cur) {
+                    prev = prev->right;
+                }
+
+                if (prev->right == NULL) {
+                    // Make a temporary link and visit node (preorder: visit before going left)
+                    preorder.push_back(cur->val);
+                    prev->right = cur;
+                    cur = cur->left;
+                } else {
+                    // Remove the temporary link
+                    prev->right = NULL;
+                    cur = cur->right;
+                }
+            }
+        }
+
+        return preorder;
     }
+
+    // \U0001f447 rename this function to match the problem name
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        solve(root, ans);
-        return ans;
+        return getPreorder(root);
     }
 };
