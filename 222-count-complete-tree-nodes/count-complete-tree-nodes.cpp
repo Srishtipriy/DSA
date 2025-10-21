b9b8295de1ca7b6c2d498f01fd4dba0919e3ead6
Srishti Priya <srishti.priya.vns@gmail.com>
2025-10-21 13:38:26 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (16.48%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -12,17 +12,8 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        queue<TreeNode* > q;
-        q.push(root);
-        int cnt = 0;
-
-        while(q.front()){
-            root = q.front();
-            q.pop();
-            cnt++;
-            q.push(root->left);
-            q.push(root->right);
-        }
-        return cnt;
-    }
+        if(root == NULL)  
+            return 0;
+        return countNodes(root->left) + countNodes(root->right) + 1;
+   }
 };
