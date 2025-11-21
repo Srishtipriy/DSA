6735e21b5e0ab11f5e55af1638d6178353dbcac0
Srishti Priya <srishti.priya.vns@gmail.com>
2025-11-21 10:25:04 +0530

    Time: 24 ms (78.28%) | Memory: 16.8 MB (59.66%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     void dfs(int i, int j, vector<vector<bool>> &vis, vector<vector<char>>& grid, int n, int m){
-        if(i<0 || j<0 || i>= n|| j>=m || vis[i][j] || grid[i][j] != '1'){
-            //vis[i][j] means already visted
-            //yaad rakho provided char grid h so->> grid[i][j] mai if!= 1 return kar do
-            return ;
-        }
+        if(i<0 || j<0 || i>= n|| j>=m || vis[i][j] || grid[i][j] != '1')
+            return;
+
         vis[i][j]=true;
 
         dfs(i-1, j  , vis , grid, n , m);    //top        
@@ -19,14 +17,12 @@ public:
         int m = grid[0].size();
 
         vector<vector<bool>> vis(n, vector<bool> (m , false));   
-        //grid banai and har yek block mai F store kiya hai
 
         for(int i=0 ; i<n ;i++){
             for(int j=0; j<m; j++){
-                //if grid mai 1 hai and not visited marked hai tbh
-                if( grid[i][j]=='1' && !vis[i][j]){ 
 
-                    dfs(i , j ,vis , grid, n, m);  //means 1 island visitho gaya
+                if( grid[i][j]=='1' && !vis[i][j]){ 
+                    dfs(i , j ,vis , grid, n, m);  //means 1 island visited
                     islands++;
                 }
             }
