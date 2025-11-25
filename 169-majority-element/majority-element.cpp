1e38163e896aa151f709aec34943941a6988a8b8
Srishti Priya <srishti.priya.vns@gmail.com>
2025-11-26 00:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (62.79%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());      //Once the array is sorted, the majority element will always be                                             present at index n/2, where n is the size of the array.
-        int n = nums.size();
-        return nums[n/2];
+        sort(nums.begin(),nums.end());
+        int mid = nums.size()/2;
+        return nums[mid];
     }
 };
