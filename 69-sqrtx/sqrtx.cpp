3dd3bd812f993a8b9b161ce35ee12481a613c63f
Srishti Priya <srishti.priya.vns@gmail.com>
2025-12-10 21:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.83%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+       if(x == 0 || x == 1)
+        return x;
+
+        int start = 1;
+        int end = x;
+        int mid ;
+
+        while(start <= end){
+            mid = start +(end - start)/2 ;
+
+            if((long)mid * mid > (long)x)       //jab mid ka sq greater ho x se, change end to mid-1
+                end = mid-1;
+            
+            else if(mid* mid == x)
+                return mid;
+            
+            else
+                start = mid +1;
+        } 
+        return floor(end);
+    }
+};
