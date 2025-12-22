eccd25e3d93386aaac3a59f24ac781442098eef4
Srishti Priya <srishti.priya.vns@gmail.com>
2025-12-23 00:01:09 +0530

    Time: 579 ms (9.25%) | Memory: 296.3 MB (24.43%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -18,32 +18,9 @@ public:
 
         return dp[curr][prev+1] = max(take, nottake);
     }
-    
-    int solveTab(int n, vector<int>& nums){
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
-        
-        for(int curr  = n-1; curr >= 0; curr --){
-            for(int prev= curr-1; prev>= -1; prev--){
-
-                int take = 0;
-                if(prev == -1 || nums[prev] < nums[curr] ){
-                    take = 1+ dp[curr+1][curr +1];
-                }
-                int notake = 0 + dp[curr+1] [prev+1];
-
-                dp[curr][prev +1] = max(take , notake);
-            }
-        }
-        return dp[0][0];
-
-    }
     int lengthOfLIS(vector<int>& nums) {
-
         int n = nums.size();
-        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        // return solve (n, nums, 0, -1, dp);
-
-        return solveTab(n, nums);
-
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return solve (n, nums, 0, -1, dp);
     }
 };
