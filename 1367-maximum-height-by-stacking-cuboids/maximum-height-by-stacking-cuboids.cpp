395438416f8bede7ec825c5de8b19494671f29e6
Srishti Priya <srishti.priya.vns@gmail.com>
2026-01-18 12:56:24 +0530

    Time: 98 ms (12.18%) | Memory: 43.4 MB (5.11%) - LeetSync

--- 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    int solve(int n, vector<vector<int>>& cuboids, int curr, int prev, vector<vector<int>>& dp) {
+    bool check(vector<int> base, vector<int> NewBox){
+        if( NewBox[0]<= base[0] && NewBox[1]<= base[1] && NewBox[2]<= base[2]){
+            return true;
+        }
+        else 
+            return false;
+            
+    }
+    int solve(int n, vector<vector<int>>& a, int curr, int prev, vector<vector<int>>& dp) {
         if (curr == n) return 0;
 
         if (dp[curr][prev + 1] != -1)
             return dp[curr][prev + 1];
 
         int take = 0;
-        if (prev == -1 ||
-            (cuboids[prev][0] <= cuboids[curr][0] &&
-             cuboids[prev][1] <= cuboids[curr][1] &&
-             cuboids[prev][2] <= cuboids[curr][2])) {
+        if (prev == -1 || check( a[curr] , a[prev]) ) {
 
-            take = cuboids[curr][2] +
-                   solve(n, cuboids, curr + 1, curr, dp);
+            take = a[curr][2] +solve(n, a, curr + 1, curr, dp);
         }
 
-        int nottake = solve(n, cuboids, curr + 1, prev, dp);
+        int nottake = solve(n, a, curr + 1, prev, dp);
 
         return dp[curr][prev + 1] = max(take, nottake);
     }
 
     int maxHeight(vector<vector<int>>& cuboids) {
         for (auto &c : cuboids)
-            sort(c.begin(), c.end());   // sort dimensions
+            sort(c.begin(), c.end());      // sort dimensions
 
         sort(cuboids.begin(), cuboids.end()); // sort cuboids
 
-        int n = cuboids.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        int n = cuboids.size();                     
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));      //dp
 
         return solve(n, cuboids, 0, -1, dp);
     }
