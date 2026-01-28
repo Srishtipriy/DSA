3e63cd0a28494c88b0be3090db798d15c426cfb4
Srishti Priya <srishti.priya.vns@gmail.com>
2026-01-28 10:36:07 +0530

    Time: 2043 ms (9.54%) | Memory: 7.6 MB (98.47%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numTrees(int n) {
+
+        if(n == 0 || n == 1)
+            return 1;
+        int ans = 0;
+        //think i as root ele
+        for(int i= 1; i<=n ; i++){
+            ans += numTrees(i-1) * numTrees(n-i);
+        }
+        return ans;
+    }
+};
