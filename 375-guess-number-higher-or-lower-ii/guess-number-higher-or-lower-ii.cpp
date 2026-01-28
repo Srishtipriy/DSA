2113a084c818b95032cb6a7ee45968273c8a3412
Srishti Priya <srishti.priya.vns@gmail.com>
2026-01-28 13:01:20 +0530

    Time: 35 ms (82.76%) | Memory: 11 MB (55.48%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    int solveMem (int start , int end ,vector<vector<int>> &dp){
-        if(start >= end)
-            return 0;
-
-        int ans  = INT_MAX;
-
-        if(dp[start][end] != -1){
-            return dp[start][end];
-        }
-        for(int i= start; i<=end ; i++){
-            ans  = min( ans , i + max( solveMem (start, i-1, dp), solveMem (i+1 , end , dp) ));
-        }
-        dp[start][end] = ans;
-        return dp[start][end];
-    }
     int solveTab(int n){
         vector<vector<int>> dp(n+2, vector<int>(n+2 , 0));
     
