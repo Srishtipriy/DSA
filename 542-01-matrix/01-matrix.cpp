3750b2ac0ffc66a7987c34c9e8230509a013bf39
Srishti Priya <srishti.priya.vns@gmail.com>
2026-03-20 10:14:05 +0530

    Time: 17 ms (62.01%) | Memory: 34.6 MB (91.79%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        if (mat.empty() || mat[0].empty())
-            return {};
+        int n = mat.size();
+        int m = mat[0].size();
 
-        int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> queue;
-        int MAX_VALUE = m * n;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    queue.push({i, j});
-                } else {
-                    mat[i][j] = MAX_VALUE;
+
+        vector<vector<int>> dp(n,vector<int>(m,INT_MAX));
+
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    dp[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
-        
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        while (!queue.empty()) {
-            auto [row, col] = queue.front(); queue.pop();
-            for (auto [dr, dc] : directions) {
-                int r = row + dr, c = col + dc;
-                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[row][col] + 1) {
-                    queue.push({r, c});
-                    mat[r][c] = mat[row][col] + 1;
+
+
+        while(!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            
+            int delX[4] = {-1,0,1,0};
+            int delY[4] = {0,1,0,-1};
+
+
+            for(int i=0;i<4;i++){
+                int newX = delX[i] + x;
+                int newY = delY[i] + y;
+                
+
+                if(newX>=0 && newX<n && newY>=0 && newY<m){
+                    if(dp[newX][newY]>1+dp[x][y]){
+                        dp[newX][newY]=1+dp[x][y];
+                        q.push({newX,newY});
+                    }
                 }
             }
+
         }
-        
-        return mat;
+        return dp;
+
     }
 };
