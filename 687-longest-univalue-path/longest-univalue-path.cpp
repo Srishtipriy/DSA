53a133e45cf202bd21294559e330e2fdfd0c6236
Srishti Priya <srishti.priya.vns@gmail.com>
2026-04-13 23:30:10 +0530

    Time: 5 ms (15.92%) | Memory: 72.2 MB (97.25%) - LeetSync

--- 687-longest-univalue-path/longest-univalue-path.cpp
+++ 687-longest-univalue-path/longest-univalue-path.cpp
@@ -14,24 +14,20 @@ public:
     int ans = 0;
 
     int checkpath(TreeNode* root) {
-        if (!root) return 0;
+        if (root == NULL) return 0;
 
         int left = checkpath(root->left);
         int right = checkpath(root->right);
 
         int leftPath = 0, rightPath = 0;
 
-        // extend path if same value
         if (root->left && root->left->val == root->val)
             leftPath = left + 1;
 
         if (root->right && root->right->val == root->val)
             rightPath = right + 1;
 
-        // update global answer
         ans = max(ans, leftPath + rightPath);
-
-        // return longest single direction
         return max(leftPath, rightPath);
     }
 
