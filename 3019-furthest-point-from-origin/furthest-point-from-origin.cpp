344e606553c81e54a553665baa318c46269d39df
Srishti Priya <srishti.priya.vns@gmail.com>
2026-04-28 00:16:37 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (63.46%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -3,12 +3,9 @@ public:
     int furthestDistanceFromOrigin(string moves) {
         int l = 0, r = 0, d = 0;
         for (char c : moves) {
-            if (c == 'L') 
-                l++;
-            else if (c == 'R') 
-                r++;
-            else 
-                d++;
+            if (c == 'L') l++;
+            else if (c == 'R') r++;
+            else d++;
         }
         return abs(l - r) + d;
     }
